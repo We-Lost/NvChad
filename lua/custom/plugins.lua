5b9d10d480f91368876f15a99e7716307f67c032
Wellinton Aracena <thewillyvegetta@gmail.com>
2024-03-02 20:28:24 -0500

    changing to random

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -43,6 +43,26 @@ local plugins = {
     opt = function()
       return vim.g.markdown_fenced_languages == { 'python', 'cpp' }
     end
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+  },
+
+  { "ray-x/go.nvim",
+    dependencies = { -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", 'gomod' },
+    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
   }
 }
 return plugins
